fabb46960c9e070e76f30ed2992ddcc85f9b9613
m-ttaylor <mttaylor@email.wm.edu>
2022-12-01 15:56:15 -0500

    Exercise 10.10

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,5 @@
+import { Platform } from "react-native";
+
 const theme = {
   colors: {
     textPrimary: '#24292e',
@@ -14,7 +16,11 @@ const theme = {
     title: 20,
   },
   fonts: {
-    main: 'System',
+    main: Platform.select({
+      android: 'Roboto',
+      ios: 'Arial',
+      default: 'system'
+    })
   },
   fontWeights: {
     normal: '400',
